ea2994bb60854d9a5fe7ab447154f52e7d6ba483
Eunji Na <neg219@naver.com>
2016-03-28 20:52:48 +0900

    #5 implement TextView Result Activity

--- app/src/main/java/kr/hs/emirim/eunji057/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/eunji057/monchicken/ResultActivity.java
@@ -10,11 +10,14 @@ import android.widget.TextView;
 public class ResultActivity extends ActionBarActivity {
 
     TextView mResult;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResult=(TextView) findViewById(R.id.result);
+        mResult.setText("결과");
     }
 
 
